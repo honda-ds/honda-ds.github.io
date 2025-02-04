134ed088f398901d1bc3300ae990d6b090833db0
Hugo Honda <hugohondaf@gmail.com>
2025-02-04 12:11:04 -0300

    update code

--- app/utils/email.ts
+++
@@ -1,9 +0,0 @@
-export const getObfuscatedEmail = () => {
-    return ['honda', '.', 'data', '.', 'science', '@', 'gmail', '.', 'com'].join('');
-};
-
-export const createEmailLink = (subject?: string) => {
-    const email = getObfuscatedEmail();
-    const encodedSubject = subject ? `?subject=${encodeURIComponent(subject)}` : '';
-    return `mailto:${email}${encodedSubject}`;
-}; 
